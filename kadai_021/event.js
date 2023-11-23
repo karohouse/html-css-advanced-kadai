a07a64f8c3be26625c32ba682d551b92ddb3eca2
karohouse <146818645+karohouse@users.noreply.github.com>
2023-11-24 08:36:23 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-
-setTimeout( function(){
-function textContent(){
-  document.getElementById('text').textContent ='ボタンをクリックしました';
-}
-document.getElementById ('btn').addEventListener('click'.textContent);
-} , 2000);
-
