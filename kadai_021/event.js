318d475340bc1170d120d247015463bd7eeef2cc
karohouse <146818645+karohouse@users.noreply.github.com>
2023-11-24 08:26:06 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+
+setTimeout( function(){
+function textContent(){
+  document.getElementById('text').textContent ='ボタンをクリックしました';
+}
+document.getElementById ('btn').addEventListener('click'.textContent);
+} , 2000);
+
